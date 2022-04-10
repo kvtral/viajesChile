f35c1413c93e54836987058d3c51841aa1201a4f
kvtral <alvaro.carrillanca@gmail.com>
2022-04-10 17:47:47 -0400

    Se agregan cambios en Javascript y en css para transición del scroll en nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 
    $(window).scroll(function() {
     if ($('#navBar').offset().top > 250) {
-        $('#navBar').addClass('bg-dark');
+        $('#navBar').addClass('bg-color');
     }  else {
-        $('#navBar').removeClass('bg-dark');
+        $('#navBar').removeClass('bg-color');
     } 
 }); 
 
@@ -14,7 +14,27 @@ $(document).ready(function () {
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
-  
 
 });
 
+function validar(){
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+
+    if (nombre === "" ||  email === "" || mensaje === "") {
+        alert("Todos los campos son obligatorios");
+    } else if (nombre.length < 3 || nombre.length > 20 || apellido.length < 3 || apellido.length > 50) {
+        alert("El nombre debe tener más de 5 caracteres y menos de 50");
+    } else if (email.indexOf("@") == -1 || email.indexOf(".") == -1) {
+        alert("El email no es valido");
+    } else if (mensaje.length < 5){
+        alert("El Mensaje debe contener más de 5 caracteres");
+    } else {  
+        alert("Mensaje enviado");
+        // pasar estos datos a funcion en lado de servidor para envio de correo
+        console.log(nombre + " | " + email + " | " + mensaje); 
+        return true;
+    }
+    return false;
+}
